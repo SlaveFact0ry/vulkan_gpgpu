0413fff85ff1d3b2fc5977caebe776ccc4df1702
SlaveFact0ry <guppy0118@gmail.com>
2025-12-09 12:23:46 +0900

    device check

--- check_device/check_device.cpp
+++ check_device/check_device.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <vulkan/vulkan.h>
- 
+
 int main() {
     VkInstanceCreateInfo createInfo = {};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
- 
+
     VkInstance instance;
     VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
     if (result != VK_SUCCESS) {
@@ -13,16 +13,29 @@ int main() {
     }
     uint32_t deviceCount = 0;
     vkEnumeratePhysicalDevices(instance, &deviceCount, NULL);
- 
+
     if (deviceCount == 0) {
       fprintf(stderr, "No GPU with Vulkan support found!\n");
       return 1;
     }
- 
+
+    std::cout << "Found " << deviceCount << " GPU(s)" << std::endl;
+
     VkPhysicalDevice* physicalDevices = new VkPhysicalDevice[deviceCount];
     vkEnumeratePhysicalDevices(instance, &deviceCount, physicalDevices);
- 
-    delete[] physicalDevices;  // 마지막에 메모리 해제
+
+    // Device 정보 출력
+    for (uint32_t i = 0; i < deviceCount; i++) {
+        VkPhysicalDeviceProperties properties;
+        vkGetPhysicalDeviceProperties(physicalDevices[i], &properties);
+        
+        std::cout << "\nDevice " << i << ":" << std::endl;
+        std::cout << "  Name: " << properties.deviceName << std::endl;
+        std::cout << "  Type: " << properties.deviceType << std::endl;
+        std::cout << "  Driver Version: " << properties.driverVersion << std::endl;
+    }
+
+    delete[] physicalDevices;
     vkDestroyInstance(instance, nullptr);
     return 0;
 }
